58e2a8e6a70aaa8e840261e597de494e79542a7b
Nuclear One <ajithsnair10@gmail.com>
2022-05-03 22:38:18 +0530

    Yup

--- src/component/Form.js
+++ src/component/Form.js
@@ -10,11 +10,27 @@ const  Form =()=>{
             color: '',
             method:'',
         },
+        validationSchema:Yup.object({
+            username:Yup.string().max(10,"Username must be <=10")
+            .min(3, 'The contact name needs to be at least 3 char')
+            .required("this is required field"),
+            name:Yup.string().required("this is required field")
+            .required("this is required field"),
+            email:Yup.string().email('Please provide valid email')
+            .required("this is required field"),
+            color:Yup.string()
+            .required("this is required field"),
+            method:Yup.string()
+            .required("this is required field")
 
+        }),
         onSubmit:(values)=>{ 
             console.log(values) }
     })
    
+    console.log(formik.errors)
+    console.log(formik.touched)
+    
     return <div>
         <form onSubmit={formik.handleSubmit}>
             <input 
@@ -22,22 +38,29 @@ const  Form =()=>{
             name="username"
             onChange={formik.handleChange}
             value={formik.values.username}
+            placeholder="Username"
+            onBlur={formik.handleBlur}
             />
+            {formik.touched.username && formik.errors.username && <p style={{color:"red"}}> {formik.errors.username }</p>}
             <input 
             type="text"
             name="name"
             onChange={formik.handleChange}
             value={formik.values.name}
+            placeholder="Name"
             />
+            {formik.touched.name && formik.errors.name && <p style={{color:"red"}}> {formik.errors.name }</p>}
             <input 
             type="email"
             name="email"
             onChange={formik.handleChange}
             value={formik.values.email}
+            placeholder="Email address"
             />
+            {formik.touched.email && formik.errors.email && <p style={{color:"red"}}> {formik.errors.email }</p>}
             <br/>
             <label> Color you like</label>
-            <br/>
+            <br />
             <label> Red</label>
             <input type="radio"
             name="color"
@@ -53,13 +76,16 @@ const  Form =()=>{
             name="color"
             value='blue'
             onChange={formik.handleChange} />
+            {formik.touched.color && formik.errors.color && <p style={{color:"red"}}> {formik.errors.color }</p>}
             <br/>
+
             <select name='method' onChange={formik.handleChange} >
                 <option value=''>select Payment Method</option>
                 <option value='Cash'>Cash</option>
                 <option value='Bitcoin'>Bitcoin</option>
                 <option value='Gold'>Gold</option>
             </select>
+            {formik.touched.method && formik.errors.method && <p style={{color:"red"}}> {formik.errors.method }</p>}
             <button type="submit">Submit</button>
         </form>
     </div>
